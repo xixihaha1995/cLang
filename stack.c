d6261b2f9c3ee9b8ec61cbeef993fcbea3a129f7
EC2 Default User <wulicheneason@gmail.com>
2020-10-25 18:00:58 +0000

    finalized

--- stack.c
+++ stack.c
@@ -7,3 +7,36 @@ Stack CreateStack(int MaxSize) {
     return S;
 }
 
+bool Push(Stack S, ElementType X, int Tag) {
+    if (S == NULL) return false;
+    if (S->Top1+1 == S->Top2) {
+        printf("Stack Full\n");
+        return false;
+    }
+    if(Tag == 1) 
+        S->Data[++S->Top1] =X;
+    else
+    {
+        S->Data[--S->Top2] = X;
+    }
+    return true;
+}
+ElementType Pop(Stack S, int Tag) {
+    if(S ==NULL)
+        return ERROR;
+    if(Tag == 1) {
+        if(S->Top1 == -1) {
+            printf("Stack 1 Empty\n");
+            return ERROR;
+        }
+        return S->Data[S->Top1--];
+    }else
+    {
+        if (S->Top2 == S->MaxSize){
+            printf("Stack 2 Empty\n");
+            return ERROR;
+        }
+        return S->Data[S->Top2++];
+    }
+    
+}
