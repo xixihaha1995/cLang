4f49f96ac22886afeba54e7159c6a03c7edc7488
EC2 Default User <wulicheneason@gmail.com>
2020-10-28 01:29:45 +0000

    pointer is primitive type or address

--- arrayPointer.c
+++ arrayPointer.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
+#include<stdlib.h>
 
 int main() {
     int arr[10];
     arr[0] = 0;
 
     int * arrPointer = (int *)malloc(10 * sizeof(int));
-
+    arrPointer[9] = &20;
+    printf("arrPointer[9] is %d\n", arrPointer[9]);
     return 0;
 
 
