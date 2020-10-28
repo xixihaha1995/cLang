60240dd5d0735d116ebde54096f07f7396854ab0
EC2 Default User <wulicheneason@gmail.com>
2020-10-28 01:57:52 +0000

    no free haha

--- arrayPointer.c
+++ arrayPointer.c
@@ -5,9 +5,38 @@ int main() {
     int arr[10];
     arr[0] = 0;
 
-    int * arrPointer = (int *)malloc(10 * sizeof(int));
-    arrPointer[9] =20;
-    printf("arrPointer[9] is %d\n", arrPointer[9]);
+    int oldSize = 5;
+    int * arrPointer = (int *)malloc(oldSize * sizeof(int));
+    if (arrPointer == NULL) {
+        printf("Memory not allocated.\n");
+        exit(0);
+    }
+    else
+    {
+        printf("Memory allocated successfully with malloc\n");
+        for (int i = 0; i < oldSize; ++i) {
+            arrPointer[i] = i + 1;
+        printf("The elements for the arrPointer is: \n");
+        for(i =0; i < n; i++) {
+            printf(" %d", arrPointer[i]);
+        }
+        printf("\n\nI'll resize the array of arrPointer\n");
+        oldSize = 6;
+        printf("Re-allocated via realloc, and the new size is %d\n", oldSize);
+        arrPointer = realloc(arrPointer, oldSize*sizeof(int) );
+        for(i = 0; i< oldSize; i++) {
+            arrPointer[i] = i+6;
+        }
+        printf("New elements are:\n");
+        for(i =0; i < oldSize; i++) {
+            printf(" %d", arrPointer[i]);
+        }
+    }
+    }
+    
+    
+    // printf("arrPointer[9] is %d\n", arrPointer[9]);
+    
     return 0;
 
 
