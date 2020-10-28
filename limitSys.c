1c8c4ffc1b7fe80ce70a114881fea8558f3e380f
EC2 Default User <wulicheneason@gmail.com>
2020-10-28 03:43:07 +0000

    system limits

---
+++ limitSys.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <limits.h>
+
+int main()
+{
+    printf("Long_Min: %ld\n", LONG_MIN);
+    printf("Long_Max: %ld\n", LONG_MAX);
+
+    printf("Char_MIN: %d\n", CHAR_MIN);
+    printf("Char_Max: %d\n", CHAR_MAX);
+
+    printf("Char_BIT: %d\n", CHAR_BIT);
+    return 0;
+
+}
