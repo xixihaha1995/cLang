a39700874e82308cca6e79f9b98a72317ad9571a
EC2 Default User <ec2-user@ip-172-31-46-8.us-east-2.compute.internal>
2020-11-05 00:56:29 +0000

    singnal

---
+++ singnal.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <signal.h>
+
+void sighandler(int);
+int main()
+{
+    signal(SIGINT, sighandler);
+    for(;;)
+    {
+        printf("sleeping ...\n");
+        sleep(3);
+    }
+}
+
+void sighandler(int signum)
+{
+    printf("caught singal %d, and now I am exiting...\n");
+}
