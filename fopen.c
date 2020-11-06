1bf46953499f18e2fc78aca81f5dd96d805005c9
EC2 Default User <wulicheneason@gmail.com>
2020-11-06 06:33:41 +0000

    aoli geigei

--- fopen.c
+++ fopen.c
@@ -1,1 +1,15 @@
-char
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    FILE *fp;
+    fp = fopen("tmp", "r");
+    if(fp == NULL)
+    {
+        fprintf(stderr, "fopen() failed errno = %d\n", errno);
+        exit(1);
+    }
+    puts("OK");
+    exit(0);
+}
