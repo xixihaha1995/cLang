72940c50456a669b01166554993b69741d093cd8
EC2 Default User <wulicheneason@gmail.com>
2020-11-06 06:52:00 +0000

    perror and fprintf

--- fopen.c
+++ fopen.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <errno.h>
+#include <string.h>
 
 int main()
 {
@@ -9,8 +10,9 @@ int main()
     int * p = malloc(sizeof(int));
     if(fp == NULL)
     {
-        // fprintf(stderr, "fopen() failed errno = %d\n", errno);
+        fprintf(stderr, "fopen() failed errno = %d\n", errno);
         perror("what's happening:");
+        fprintf(stderr, "fopen(): %s\n", strerror(errno));
         exit(1);
     }
     puts("OK");
