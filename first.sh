f9832859c6afbf6737e50597ae9cc09a73b810d6
EC2 Default User <wulicheneason@gmail.com>
2020-11-10 03:52:00 +0000

    sh two parameters before passing

--- first.sh
+++ first.sh
@@ -1,3 +1,12 @@
 #!/bin/sh
 #this is a comment
+echo "Hello      World"       # This is a comment, too!
+echo "Hello World"
+echo "Hello * World"
+echo Hello * World
 echo Hello      World
+echo "Hello" World
+echo Hello "     " World
+echo "Hello "*" World"
+echo `hello` world
+echo 'hello' world
