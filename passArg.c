908f6c331cfb69fd18d1cc4bff000374bc62fd0b
EC2 Default User <wulicheneason@gmail.com>
2020-11-12 07:46:00 +0000

    argv should be printed via fprintf

--- passArg.c
+++ passArg.c
@@ -6,10 +6,10 @@ int main(int argc, char **argv)
     printf("Argc is %d\n", argc);
     puts("Argv\n");
     // fprintf(stdout, "%s\n", argv[0]);
-    // for(int i = 1; i < argc; i++)
-    // {
-    //     printf("%s ", argv[i]);
-    // }
+    for(int i = 0; i < argc; i++)
+    {
+        fprintf(stdout, "%s ", argv[i]);
+    }
 
 
     exit(0);
