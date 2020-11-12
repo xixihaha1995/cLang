85a97a13cdd1f1eb1e909328d26e229c83ccc6f1
EC2 Default User <wulicheneason@gmail.com>
2020-11-12 07:48:48 +0000

    argv for parameters including exe name

--- mycpy.c
+++ mycpy.c
@@ -8,12 +8,12 @@ int main(int argc, char **argv)
     
     if(argc < 3)
     {
-        fprintf(stderr, "%s <src_file> <dst_file>\n", argv[0]);
+        fprintf(stderr, "Usage: %s <src_file> <dst_file>\n", argv[0]);
         exit(1);
     }
     fps = fopen(argv[1], "r");
     if (fps == NULL){
-        perror();
+        perror("fopen");
         exit(1);
     }
     fpd = fopen(argv[2], "w");
