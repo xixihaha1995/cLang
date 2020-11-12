c497fdbb84789307f9fa9494a097b328315c82d6
EC2 Default User <wulicheneason@gmail.com>
2020-11-12 07:18:02 +0000

    mode is a string

--- mycpy.c
+++ mycpy.c
@@ -6,12 +6,16 @@ int main(int argc, char **argv)
     FILE *fps, *fpd;
     int ch;
 
-    fps = fopen(argv[1], 'r');
-    fpd = fopen(argv[2], 'w');
+    fps = fopen(argv[1], "r");
+    fpd = fopen(argv[2], "w");
 
     while(1)
     {
         ch = fgetc(fps);
+        if (ch == EOF)
+        {
+            break;
+        }
         fputc(ch, fpd);
     }
     fclose(fps);
