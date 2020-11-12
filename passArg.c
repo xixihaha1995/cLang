2802d57a849c93bfa414f8f922b9e46c32c8a949
EC2 Default User <wulicheneason@gmail.com>
2020-11-12 07:41:37 +0000

    argv should be printed directly

--- passArg.c
+++ passArg.c
@@ -3,12 +3,11 @@
 
 int main(int argc, char **argv)
 {
-    puts("Argc %d\n");
-    putchar(argc);
+    printf("Argc is %d\n", argc);
     puts("Argv\n");
-    for(int i = 0; i < argc - 1; i++)
+    for(int i = 1; i < argc; i++)
     {
-        printf("%s", *argv[i]);
+        printf("%s ", argv[i]);
     }
 
 
