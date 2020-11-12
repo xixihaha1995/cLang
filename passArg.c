b7a15af6c2e5b2fbe9a0a9dd2562b283730b8a37
EC2 Default User <wulicheneason@gmail.com>
2020-11-12 07:34:35 +0000

    pointer is value address, * value, &pointer  pointer address

--- passArg.c
+++ passArg.c
@@ -3,9 +3,14 @@
 
 int main(int argc, char **argv)
 {
-    puts("Argc %d\n",);
+    puts("Argc %d\n");
     putchar(argc);
     puts("Argv\n");
-    puts(argv);
+    for(int i = 0; i < argc - 1; i++)
+    {
+        printf("%s", *argv[i]);
+    }
+
+
     exit(0);
 }
