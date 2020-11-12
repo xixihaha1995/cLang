c0f545484bb844446d4d1ddb8d3c9f3f06b10ec6
EC2 Default User <wulicheneason@gmail.com>
2020-11-12 07:11:51 +0000

    continus get and put

--- mycpy.c
+++ mycpy.c
@@ -3,5 +3,19 @@
 
 int main(int argc, char **argv)
 {
-    
+    FILE *fps, *fpd;
+    int ch;
+
+    fps = fopen(argv[1], 'r');
+    fpd = fopen(argv[2], 'w');
+
+    while(1)
+    {
+        ch = fgetc(fps);
+        fputc(ch, fpd);
+    }
+    fclose(fps);
+    fclose(fpd);
+
+    exit(0)
 }
