c9d3a603b2ddcb885bafaa748227b9839c7acb6f
EC2 Default User <wulicheneason@gmail.com>
2020-11-13 02:22:59 +0000

    how FILE pointer move

---
+++ mycpy_fget.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define BUFFSIZE 1024
+int main(int argc, char **argv)
+{
+    FILE *fps, *fpd;
+    char buf[BUFFSIZE];    
+    if(argc < 3)
+    {
+        fprintf(stderr, "Usage: %s <src_file> <dst_file>\n", argv[0]);
+        exit(1);
+    }
+    fps = fopen(argv[1], "r");
+    if (fps == NULL){
+        perror("fopen");
+        exit(1);
+    }
+    fpd = fopen(argv[2], "w");
+    if (fpd == NULL){
+        fclose(fps);
+        perror("fopen");
+        exit(1);
+    }
+    while(fgets(buf, BUFFSIZE, fps) != NULL)
+    {
+        fprintf(stdout, "fps address %d\n", fps);
+        fputs(buf, fpd);
+    }
+        
+    fclose(fps);
+    fclose(fpd);
+
+    exit(0);
+}
