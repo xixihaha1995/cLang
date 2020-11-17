46bf7a0139c389a871508e1f5118550b697b7723
EC2 Default User <wulicheneason@gmail.com>
2020-11-17 05:56:25 +0000

    dot command

--- shellScripting/var2.sh
+++ shellScripting/var2.sh
@@ -2,3 +2,7 @@
 echo "MYVAR IS $MYVAR"
 MYVAR="hi there"
 echo "MYVAR IS $MYVAR"
+#source a scipt wia the "."(dot) command, to receive variables changes from
+#the scipt
+#this export is not required, dot means force scipt runing within our own 
+#interactive shell
