33d36fd840485ebde87d10276c0fda8de765e56a
EC2 Default User <wulicheneason@gmail.com>
2020-11-22 21:07:49 +0000

    that's all time for pr7

--- passArg.c
+++ passArg.c
@@ -7,14 +7,13 @@ int main(int argc, char **argv)
     printf("Argc is %d\n", argc);
     puts("Argv is ");
     // fprintf(stdout, "%s\n", argv[0]);
-    int* poinInt = (int *)malloc(1);
-    poinInt = 1;
+
 
     for(int i = 1; i < argc; i++)
     {
         fprintf(stdout, "%s ", argv[i]);
     }
-    free(poinInt);
+
 
     exit(0);
 }
