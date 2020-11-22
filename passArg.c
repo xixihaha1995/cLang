42aa0933d0a4d5982361ffdabeb12bf918758c45
lichenFrom HP <wulicheneason@gmail.com>
2020-11-22 13:41:35 -0700

    gdb is very nice

--- passArg.c
+++ passArg.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int main(int argc, char **argv)
 {
     printf("Argc is %d\n", argc);
-    puts("Argv\n");
+    puts("Argv is ");
     // fprintf(stdout, "%s\n", argv[0]);
-    for(int i = 0; i < argc; i++)
+    for(int i = 1; i < argc; i++)
     {
         fprintf(stdout, "%s ", argv[i]);
     }
