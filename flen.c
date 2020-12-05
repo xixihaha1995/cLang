34db490c0a9284964968a2ae39fdfa74f4543e68
EC2 Default User <wulicheneason@gmail.com>
2020-12-05 03:11:40 +0000

    fseek and ftell

--- flen.c
+++ flen.c
@@ -1,8 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(argc, argv)
+int main(int argc, char ** argv)
 {
-    File * fp;
-    
+    FILE * fp;
+    fp = fopen(argv[1], "r");
+    if (fp == 0) {
+        perror("fopen()");
+        exit(1);
+    }
+    fseek(fp, 0, SEEK_END);
+    printf("%ld\n", ftell(fp));
+    return 0;
+
 }
