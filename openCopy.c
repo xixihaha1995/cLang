d6f65ca6614baf4e58eca8a4a62081813682e03c
EC2 Default User <wulicheneason@gmail.com>
2020-12-06 23:48:00 +0000

    open close read write

---
+++ openCopy.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define BUFFSIZE 1024
+
+int main(int argc, char * argv)
+{
+    int sfd, dfd;
+    int rc, wc;
+    char buf[BUFFSIZE];
+    open();
+    open();
+
+    while(1)
+    {
+        rc = read();
+        write();
+    }
+
+    close();
+    close();
+
+}
