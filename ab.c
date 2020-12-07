3f6d0658b1929448bfe5c02351aa2455c13de7b7
EC2 Default User <wulicheneason@gmail.com>
2020-12-07 19:07:05 +0000

    ab for sysio or stdio

---
+++ ab.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int main()
+{
+    putchar('a');
+    fflush(NULL);
+    write(1,"b",1);
+
+    putchar('a');
+    write(1,"b",1);
+
+    putchar('a');
+    write(1,"b",1);
+
+    exit(0);
+}
