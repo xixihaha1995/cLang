8093f68601d2ae0ab9add7bf4b119c40a014cec8
EC2 Default User <wulicheneason@gmail.com>
2020-12-09 05:38:37 +0000

    stat() stat cmd stat struct

---
+++ fs.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+
+static int flen(const char * fname)
+{
+    struct stat statres;
+    if (stat(fname,&statres)<0)
+    {
+        perror("stat()");
+        exit(1);
+    }
+    return statres.st_size;
+}
+
+int main(int argc, char ** argv)
+{
+    if (argc < 2)
+    {
+        perror("Usage [FILENAME]");
+        exit(1);
+    }
+    printf("%d\n", flen(argv[1]));
+    exit(1);
+}
