658dde52e0dfe312cd51f835f23a734146b57008
awsRemote <wulicheneason@gmail.com>
2020-12-15 22:26:51 +0000

    secondary pointer and first pointer

--- dlsym.c
+++ dlsym.c
@@ -7,5 +7,17 @@ int main(int argc, char **argv)
     void *handle;
     double (*cosine)(double);
     // the above function pointer, where input is double and outpur is also double
+    char *error;
+    handle = dlopen("libm.so", RTLD_LAZY);
+    if (!handle)
+    {
+        fprintf(stderr, "%s\n", dlerror());
+        exit(EXIT_FAILURE);
+    }
+    dlerror();
+    *(void **)(&cosine)= dlsym(handle, "cos");
+    // address of funciton pointer (&cosine) secondary pointer
+    // (void **) force secondary pointer to a void secondary pointer
+    // value of void secondary pointer is void first pointer
     exit(0);
 }
