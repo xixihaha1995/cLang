41eaf6f36722e695d94293c2166866c64376a949
awsRemote <wulicheneason@gmail.com>
2020-12-15 22:13:26 +0000

    function pointer

---
+++ dlsym.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <dlfcn.h>
+
+int main(int argc, char **argv)
+{
+    void *handle;
+    double (*cosine)(double);
+    // the above function pointer, where input is double and outpur is also double
+    exit(0);
+}
