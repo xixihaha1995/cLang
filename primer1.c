4b7d252be016b8bf705a143a29701b32f250cad1
awsRemote <wulicheneason@gmail.com>
2020-12-18 05:07:18 +0000

    garbage collection

---
+++ primer1.c
@@ -0,0 +1,44 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+
+#define LEFT 30000000
+#define RIGHT 30000200
+
+int main()
+{
+    int i, j, mark;
+    pid_t pid;
+    for(i=LEFT; i<=RIGHT;++i)
+    {   
+        pid = fork();
+        if (pid < 0)
+        {
+            perror("fork()");
+            exit(1);
+        }
+        if (pid  == 0)
+        {
+            mark =1;
+            for(j=2; j < i/2; ++j)
+            {
+                if (i%j == 0)
+                {
+                    mark = 0;
+                    break;
+                }
+            }
+            if (mark == 1)
+            {
+                printf("%d is a primer\n", i);
+            }
+            exit(0);
+        }
+        for(i = LEFT; i <= RIGHT; ++i)
+        {
+            wait(NULL);
+        }
+        
+    }
+    exit(0);
+}
