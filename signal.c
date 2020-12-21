468860616587c87130b534da1fb694cb43750e27
awsRemote <wulicheneason@gmail.com>
2020-12-21 06:27:26 +0000

    ctrl + c is SIGINT

---
+++ signal.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <signal.h>
+#include <unistd.h>
+
+static void interRupt(int sig)
+{
+    printf("caught singal %d, I'm exiting\n", sig);
+    exit(1);
+}
+
+
+int main()
+{
+    int i;
+    signal(SIGINT, interRupt);
+    for (i=0; i < 10; i++)
+    {
+        write(1,"*",1);
+        sleep(1);
+    }
+    exit(0);
+}
