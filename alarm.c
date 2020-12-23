3c963cd8fcc320fa2328b72bcf905f99ab49cf59
awsRemote <wulicheneason@gmail.com>
2020-12-23 00:37:37 +0000

    volatile for real value of int

--- alarm.c
+++ alarm.c
@@ -1,6 +1,24 @@
 #include <stdlib.h>
+#include <stdio.h>
+#include <signal.h>
+#include <unistd.h>
+
+
+static volatile int loop = 1;
+static void alarm_handler(int s)
+{
+    loop = 0;
+}
 
 int main()
 {
-    time_t end;
+    int64_t count = 0;
+    alarm(5);
+    // after 5 seconds, this program will end.
+    signal(SIGALRM, alarm_handler);
+    // replace alarm signal behavior from end to user-defined
+    while(loop)
+        count ++;
+    printf("%lld", count);
+    exit(0);
 }
