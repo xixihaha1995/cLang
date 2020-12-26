f556bd4e308921d79236f70f9640843125059e77
awsRemote <wulicheneason@gmail.com>
2020-12-26 03:44:14 +0000

    reverse hook call

--- thread/cleanup.c
+++ thread/cleanup.c
@@ -1,4 +1,43 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <pthread.h>
-#include <string.h>s
+#include <string.h>
+
+
+static void clean_func(void *p)
+{
+    puts(p);
+}
+
+static void *func(void *p)
+{
+    puts("Thread is working!");
+
+    pthread_cleanup_push(clean_func,"reverse call: 1");
+    pthread_cleanup_push(clean_func,"reverse call: 1");
+    pthread_cleanup_push(clean_func,"reverse call: 1");
+
+    puts("push over!");
+
+    pthread_cleanup_pop(1);
+    pthread_cleanup_pop(1);
+    pthread_cleanup_pop(1);
+
+
+    pthread_exit(NULL);
+    // return NULL;
+}
+
+int main()
+{
+    pthread_t tid;
+    int err;
+    puts("Begin!");
+    err = pthread_create(&tid, NULL, func, NULL);
+    if(err)
+    {
+        fprintf(stderr, "pthread_create():%s\n", strerror(err)); 
+        exit(1);
+    }
+    exit(0);
+}
