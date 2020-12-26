9fe45b9cec45a0ea04159db8a91a585cba25f0e1
awsRemote <wulicheneason@gmail.com>
2020-12-26 04:37:07 +0000

    thread based primer selection

---
+++ thread/primThread.c
@@ -0,0 +1,41 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+
+#define LEFT 30000000
+#define RIGHT 30000200
+#define THRNUM (RIGHT-LEFT+1)
+
+int main()
+{
+    int i, j, mark;
+    pid_t pid;
+    for(i=LEFT; i<=RIGHT;++i)
+    {   
+        pid = fork();
+        if (pid < 0)
+        {
+            perror("fork()");
+            exit(1);
+        }
+        if (pid  == 0)
+        {
+            mark =1;
+            for(j=2; j < i/2; ++j)
+            {
+                if (i%j == 0)
+                {
+                    mark = 0;
+                    break;
+                }
+            }
+            if (mark == 1)
+            {
+                printf("%d is a primer\n", i);
+            }
+            exit(0);
+        }
+        
+    }
+    exit(0);
+}
