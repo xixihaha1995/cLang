49868f6da51bc2e5c1035520c4e62f29997ca169
awsRemote <wulicheneason@gmail.com>
2020-12-27 04:35:29 +0000

    deadlock avoided

--- thread/primPool.c
+++ thread/primPool.c
@@ -8,7 +8,6 @@
 #define RIGHT 30000200
 #define THRNUM 4
 
-
 static int num = 0;
 static pthread_mutex_t mut_num = PTHREAD_MUTEX_INITIALIZER;
 
@@ -20,15 +19,13 @@ struct thr_arg_st
 
 int main()
 {
-    int i,err;
+    int i, err;
     pthread_t tid[THRNUM];
     struct thr_arg_st *p;
     void *returnedStP;
-    
-    
 
-    for(i=0; i<THRNUM;i++)
-    {   
+    for (i = 0; i < THRNUM; i++)
+    {
         p = malloc(sizeof(*p));
         if (p == NULL)
         {
@@ -36,19 +33,19 @@ int main()
             exit(1);
         }
         p->n = i;
-        err = pthread_create(tid+i, NULL,thr_prime,p);
+        err = pthread_create(tid + i, NULL, thr_prime, p);
         if (err)
         {
             fprintf(stderr, "pthread_create():%s\n", strerror(err));
             exit(1);
         }
     }
-    
-    for(i = LEFT; i<= RIGHT; i++)
+
+    for (i = LEFT; i <= RIGHT; i++)
     {
         // main 线程为什么还能上锁，解锁
         pthread_mutex_lock(&mut_num);
-        while(num != 0)
+        while (num != 0)
         {
             pthread_mutex_unlock(&mut_num);
             // small sleep without interuption of process scheduling
@@ -57,11 +54,10 @@ int main()
         }
         num = i;
         pthread_mutex_unlock(&mut_num);
-
     }
     num = -1;
 
-    for(i = 0; i<= THRNUM; i++)
+    for (i = 0; i <= THRNUM; i++)
     {
         // join needs a secondary pointer
         // or the address of void *pointer
@@ -70,43 +66,49 @@ int main()
     }
 
     pthread_mutex_destroy(&mut_num);
-        
-    
+
     exit(0);
 }
 // void *p is exactly &i from which it has been called in main function
 static void *thr_prime(void *p)
 {
-    int mark,i,j;
+    int mark, i, j;
     // force void * to struct * pointer, then get its member n
     // i = ((struct thr_arg_st *)p)->n;
 
-
-    pthread_mutex_lock(&mut_num);
-    while(num == 0)
+    while (1)
     {
-        pthread_mutex_unlock(&mut_num);
-        sched_yield();
         pthread_mutex_lock(&mut_num);
-    }
-    i = num;
-    num = 0;
-    pthread_mutex_unlock(&mut_num);
-
-    // free(p);
-    mark = 1;
-    for(j = 2; j<i/2; j++)
-    {
-        if(i%j == 0)
+        while (num == 0)
         {
-            mark = 0;
+            pthread_mutex_unlock(&mut_num);
+            sched_yield();
+            pthread_mutex_lock(&mut_num);
+        }
+        if (num == -1)
+        {
+            pthread_mutex_lock(&mut_num);
             break;
         }
+        i = num;
+        num = 0;
+        pthread_mutex_unlock(&mut_num);
+
+        // free(p);
+        mark = 1;
+        for (j = 2; j < i / 2; j++)
+        {
+            if (i % j == 0)
+            {
+                mark = 0;
+                break;
+            }
+        }
+        if (mark)
+        {
+            printf("%d is a primer\n", i);
+        }
     }
-    if(mark)
-    {
-        printf("%d is a primer\n", i);
-    }
+
     pthread_exit(p);
-    
 }
