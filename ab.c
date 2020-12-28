0cfd0c0238c2779d9a62af28a49277699d0c741c
awsRemote <wulicheneason@gmail.com>
2020-12-28 03:31:25 +0000

    p is var's addr, *p is its value, &p is p's addr

--- ab.c
+++ ab.c
@@ -4,15 +4,20 @@
 
 int main()
 {
-    putchar('a');
-    fflush(NULL);
-    write(1,"b",1);
+    // putchar('a');
+    // fflush(NULL);
+    // write(1,"b",1);
 
-    putchar('a');
-    write(1,"b",1);
+    // putchar('a');
+    // write(1,"b",1);
 
-    putchar('a');
-    write(1,"b",1);
+    // putchar('a');
+    // write(1,"b",1);
+    int *p;
+    int num;
+    num = 5;
+    p = &num;
+    fprintf(stdout,"test for  pointer:%d, and its value:%d\n",p,*p);
 
     exit(0);
 }
