2b78df21568e1e70cd470f69b84644b82ea76305
awsRemote <wulicheneason@gmail.com>
2020-12-31 23:11:22 +0000

    might  be wrong with typedef

--- socket/dgram/proto.h
+++ socket/dgram/proto.h
@@ -1,3 +1,12 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
 #define RCVPORT     "1989"
-#define NAMEMAX     512
+#define NAMEMAX     512-8-8
 
+struct msg_st
+{
+    u_int32_t math;
+    u_int32_t chinese;
+    u_int8_t name[1];
+}__attribute__((packed));
