f0478efddca2998d0c3c1c358730c9331a4754cd
awsRemote <wulicheneason@gmail.com>
2021-01-26 06:47:54 +0000

    wrong with formal parameters

--- mytbf/mytbf.c
+++ mytbf/mytbf.c
@@ -13,14 +13,17 @@ mytbf_t *mytbf_init(int cps, int burst)
 {
 
 }
-int     mytbf_fetchtoken(mytbf_t *, int)
+
+int     mytbf_fetchtoken(mytbf_t *, int size)
 {
 
 }
+
 int     mytbf_returntoken(mytbf_t *, int)
 {
 
 }
+
 int     mytbf_destroy(mytbf_t *)
 {
     
