4319e7009a2fc88116fded03d86c018990459fe5
awsRemote <wulicheneason@gmail.com>
2021-01-27 06:10:07 +0000

    WHY MY strtok_r is segmentation fault

--- strtok.c
+++ strtok.c
@@ -1,15 +1,33 @@
-#include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
 
 int main()
 {
     char *str= "GETFILE GET /path/to/file\r\n\r\n";
-    char *token;
+    char *token =NULL;
     char *saveptr;
-    for(token = strtok_r(str," ",&saveptr); token != NULL; token = strtok_r(NULL," ",&saveptr))
+    for(token = strtok_r(str," ",&saveptr); token; token = strtok_r(NULL," ",&saveptr))
     {
         printf("%s\n", token);
     }
     return 0;
 }
+
+// #include <stdio.h>
+// #include <string.h>    
+
+// int main(void)
+// {
+//     char hello[] = "Hello World, let me live.";
+
+//     char *tmp;
+//     char *token = NULL;
+//     for(token = strtok_r(hello, ", ", &tmp);
+//         token != NULL;
+//         token = strtok_r(NULL, ", ", &tmp))
+//     {
+//         printf("%s\n", token);
+//     }
+
+//     return 0;
+// }
