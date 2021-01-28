770c2883e72cb6b2a0ebef9773224b4439b7cf5b
awsRemote <wulicheneason@gmail.com>
2021-01-28 01:21:27 +0000

    static int and its first pointer and seconder pointer

--- struct.c
+++ struct.c
@@ -13,11 +13,20 @@ struct out
 
 };
 
+static void example(int **x)
+{
+    printf("I got a number:%d from a double pointer\n", **x);
+}
+
 int main()
 {
-    struct out outsider;
+    // struct out outsider;
     // outsider = malloc(sizeof(*outsider));
-    outsider.a = 666;
-    printf("The num insider which inside out is: %d\n", outsider.a);
+    // outsider.a = 666;
+    // printf("The num insider which inside out is: %d\n", outsider.a);
+    int a = 666;
+    int *aP = &a;
+    // int **aPP = &(&(a));
+    example(&aP);
     return 0;
 }
