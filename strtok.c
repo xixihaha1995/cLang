82fcbdfc964b3a519b93a6d3df4b21b91fd198eb
awsRemote <wulicheneason@gmail.com>
2021-01-31 02:55:34 +0000

    strtok can parse without segmation fault

--- strtok.c
+++ strtok.c
@@ -3,14 +3,22 @@
 
 int main()
 {
-    char str[] = "GETFILE GET /path/to/file\r\n\r\n123456";
-    char *token[3];
+    char str[] = "GETFILE GET";
+    char *token[5];
     char *saveptr;
     int i = 0;
-    for(token[i] = strtok_r(str," ",&saveptr); token[i] != NULL; token[++i] = strtok_r(NULL," ",&saveptr))
+    for(token[i] = strtok_r(str," ",&saveptr); ; token[++i] = strtok_r(NULL," ",&saveptr))
     {
-        printf("token[%d]: %s\n", i,token[i]);
+        if (token[i] == NULL)
+            break;
+         printf("token[%d]: %s\n", i,token[i]);
     }
+    printf("%d\n", i);
+    if(i == 3 && token[0] == "GETFILE" && token[1] == "GET" && token[2][0]=='/' )
+    {
+        printf("token[%d]: %c\n", 2,token[2][0]);
+    }
+
     return 0;
 }
 
