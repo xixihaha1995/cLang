b85d4c72548b92424dd174afd14d578f4bd016a1
awsRemote <wulicheneason@gmail.com>
2021-02-06 03:27:51 +0000

    mytbf-mt token array is 临界区 独占

---
+++ mytbf-mt/main.c
@@ -0,0 +1,109 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <signal.h>
+#include <string.h>
+#include "mytbf.h"
+
+
+#define CPS 10
+#define BUFSIZE 1024
+#define BURST   100
+
+
+int main(int argc, char **argv)
+{
+    int sfd, dfd = 1;
+    char    buf[BUFSIZE];
+    int len, ret, pos;
+    int size;
+    mytbf_t *tbf;
+
+    if (argc < 2)
+    {
+        fprintf(stderr, "Usage ...\n" );
+        exit(1);
+    }
+    tbf = mytbf_init(CPS,BURST);
+    if(tbf == NULL)
+    {
+        fprintf(stderr,"mytbf_init\n");
+        exit(1);
+    }
+    // prevention of signal interrupt
+    do
+    {
+        sfd = open(argv[1], O_RDONLY);
+        if (sfd < 0)
+        {
+            if(errno != EINTR)
+            {
+                perror("open ");
+                exit(1);
+            }
+        }
+    }while(sfd < 0);
+
+    while(1)
+    {
+        size = mytbf_fetchtoken(tbf, BUFSIZE);
+        if(size <0 )
+        {
+            fprintf(stderr,"cannot fetch valid token: %s\n", strerror(-size));
+            exit(1);
+        }
+
+        // token is used for how much you can read
+        while((len = read(sfd,buf,size)) < 0)
+        {
+            if(errno == EINTR)
+            {
+                continue;
+            }
+            perror("read");
+            break;
+        }
+        
+        if((size - len) > 0)
+        {
+            if((mytbf_returntoken(tbf,size-len)) < 0)
+            {
+                fprintf(stderr,"cannot fetch valid token: %s\n", strerror(-size));
+                exit(1);
+            }
+        }
+
+        if(len == 0)
+        {
+            break;
+        }
+        pos = 0;
+        while(len >0)
+        {
+            ret = write(dfd,buf+pos,len);
+            if(ret <0)
+            {
+                if(errno == EINTR)
+                {
+                    continue;
+                }
+                perror("write");
+                break;
+            }
+            if(ret == 0)
+            {
+                break;
+            }
+            len -= ret;
+            pos += ret;
+        }
+    }
+    close(sfd);
+    mytbf_destroy(tbf);
+
+    return 0;
+}
---
+++ mytbf-mt/mytbf.c
@@ -0,0 +1,136 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <signal.h>
+#include <unistd.h>
+#include "mytbf.h"
+
+// 令牌桶指针数组什么时候被创建
+// 当 mytbf[ch]被编译链接后
+static struct mytbf_st* job[MYTBF_MAX];
+static int inited = 0;
+
+struct mytbf_st
+{
+    int cps;
+    int burst;
+    int token;
+    int pos;
+};
+static int get_free_pos(void)
+{
+    for(int i = 0; i < MYTBF_MAX; i++)
+    {
+        if(job[i] == NULL)
+            return i;
+    }
+    return -1;
+}
+
+static int min(int a, int b)
+{
+    if(a < b)
+        return a;
+    return b;
+}
+static __sighandler_t alrm_handler_save;
+
+static void alrm_handler(int s)
+{
+    // 经典嵌套调用：alrm_handler因为alarm信号才回调，马上又预约下一个alarm
+    alarm(1);
+    for(int i = 0; i< MYTBF_MAX; i++)
+    {
+
+        // 闲着没事，给所有在使用的tbf加token，每次加cps
+        if(job[i] != NULL)
+        {
+            job[i]->token += job[i]->cps;
+            if (job[i]->token > job[i]->burst)
+                job[i]->token = job[i]->burst;
+        }
+    }
+}
+
+static void module_unloader(void)
+{
+    signal(SIGALRM, alrm_handler_save);
+    alarm(0);
+    for(int i = 0; i < MYTBF_MAX; i++)
+        free(job[i]);
+    
+}
+
+static void module_loader(void)
+{
+    alrm_handler_save = signal(SIGALRM, alrm_handler);
+    alarm(1);
+    atexit(module_unloader);
+}
+
+mytbf_t *mytbf_init(int cps, int burst)
+{
+    struct mytbf_st *me;
+    if(!inited)
+    {
+        module_loader();
+        inited = 1;
+    }
+
+
+    int pos;
+    me = malloc(sizeof(*me));
+    if(me  == NULL)
+    {
+        return NULL;
+    }
+    me->cps = cps;
+    me->burst = burst;
+    me->token = 0;
+    pos = get_free_pos();
+    if(pos < 0)
+        return NULL;
+    me->pos = pos;
+    job[pos] = me;
+
+    return me;
+}
+
+int     mytbf_fetchtoken(mytbf_t *ptr, int size)
+{   
+    if(size <= 0)
+        return -EINVAL;
+    struct mytbf_st *me = ptr;
+    int len;
+    while(me->token <= 0)
+        pause();
+        // 等待分配token
+    len = min(me->token, size);
+    me->token -= len;
+    return len;
+}
+
+
+//token银行，用来存储token（权限） 
+int     mytbf_returntoken(mytbf_t *ptr, int size)
+{
+    if(size <= 0)
+    {
+        return -EINVAL;
+    }
+    struct mytbf_st *me = ptr;
+    me->token += size;
+    if(me->token > me->burst)
+        me->token = me->burst;
+        // basically information can be passed via ptr from main.c to mytbf.c;
+
+    return size;
+}
+
+int     mytbf_destroy(mytbf_t *ptr)
+{
+    struct mytbf_st *me = ptr;
+    job[me->pos] = NULL;
+    free(me);
+    return 0;
+}
