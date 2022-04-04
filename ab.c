d5207d3324ebd15ac19f1830b2598670004acc58
Lichen Wu <43649779+xixihaha1995@users.noreply.github.com>
2022-04-04 10:44:23 -0600

    vs solution is a window for working github code on windows locally

--- ab.c
+++ ab.c
@@ -1,18 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
 
 int main()
 {
-    // putchar('a');
-    // fflush(NULL);
-    // write(1,"b",1);
-
-    // putchar('a');
-    // write(1,"b",1);
-
-    // putchar('a');
-    // write(1,"b",1);
     int *p;
     int num;
     num = 5;
