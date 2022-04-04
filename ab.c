b8d662aa93cb844d0a6e77bf8fa6d7f23128298f
Lichen Wu <43649779+xixihaha1995@users.noreply.github.com>
2022-04-04 10:54:14 -0600

    debug fprintf

--- ab.c
+++ ab.c
@@ -7,7 +7,6 @@ int main()
     int num;
     num = 5;
     p = &num;
-    fprintf(stdout,"test for  pointer:%d, and its value:%d\n",p,*p);
-
-    exit(0);
+    fprintf(stdout,"test for  pointer:%p, and its value:%d\n",p,*p);
+    return 0;
 }
